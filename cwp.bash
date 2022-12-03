dd27cbcf8a8323e91ffd818ec0dffbd1acbb513e
Kasper Gałkowski <kpg@posteo.net>
2022-12-03 12:58:52 +0100

    add uberjar helper

--- cwp.bash
+++ cwp.bash
@@ -15,3 +15,12 @@ cwp+docker () {
         --expr "with builtins; with getFlake \"$(pwd)\"; (getAttr currentSystem packages).clojure.withPackages (ps: with ps; [ $pkgs ])"
 }
 
+cwp+uberjar () {
+    local name=$1;
+    shift
+    local pkgs=$@;
+    nix build \
+        --impure \
+        --expr "with builtins; with getFlake \"$(pwd)\"; let clojure = (getAttr currentSystem packages).clojure; in clojure.buildUberjar \"$name\" (with clojure.pkgs; [ $pkgs ])"
+}
+
