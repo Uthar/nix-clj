041101d8cb0ead49d4fcc79d141ba29b2f8449c0
Kasper Gałkowski <kgalkowski@comscore.com>
2023-09-11 14:35:22 +0200

    WIP leiningen hashed tarball

--- fetch-deps-tarball.nix
+++ fetch-deps-tarball.nix
@@ -1,4 +1,4 @@
-# { hash, src, ... }:
+# { hash, src,4 ... }:
 { pkgs ? import <nixpkgs> {}, ... }:
 
 let
@@ -16,7 +16,7 @@ let
 
       nativeBuildInputs = with pkgs; [
         tree
-        clojure
+        leiningen
         git
         cacert
       ];
@@ -59,6 +59,49 @@ let
       
     };
 
+  fetchLeiningenTarball = { src, m2Sha256 }:
+
+    pkgs.stdenvNoCC.mkDerivation {
+
+      outputHashMode = "flat";
+      outputHashAlgo = "sha256";
+      outputHash = m2Sha256;
+
+      pname = "m2";
+      version = "tarball";
+      inherit src;
+
+      nativeBuildInputs = with pkgs; [
+        tree
+        leiningen
+        cacert
+      ];
+
+      buildPhase = ''
+        export SOURCE_DATE_EPOCH=1
+        export HOME=$(pwd)
+
+        lein deps
+
+        tree -a /build/.m2/
+
+        cat /build/.m2/repository/org/postgresql/postgresql/42.6.0/_remote.repositories
+
+        # contains impure timestamp
+        find /build/.m2 -name _remote.repositories -exec rm -v {} \; || true
+      '';
+
+
+      installPhase = ''
+        ls -lah .
+        ls -lah /build
+        tar --owner=0 --group=0 --numeric-owner --format=gnu \
+            --sort=name --mtime="@$SOURCE_DATE_EPOCH" \
+            -czf $out /build/.m2
+      '';
+      
+    };
+
   buildClojurePackage = { pname, version, src, m2Sha256 }:
     let
       m2 = fetchDepsEdnTarball { inherit src m2Sha256; };      
@@ -85,10 +128,37 @@ let
       '';
     };
 
-in buildClojurePackage {
-  pname = "test";
-  version = "test";
-  src = ./.;
-  # m2Sha256 = "sha256-PGtBTUE/A6zEpasnmxRuzMnOCjGq2hsgCkHg3M5ZiAQ=";
-  m2Sha256 = "";
-}
+  buildClojurePackage2 = { pname, version, src, m2Sha256 }:
+    let
+      m2 = fetchLeiningenTarball { inherit src m2Sha256; };      
+    in pkgs.stdenvNoCC.mkDerivation {
+      inherit pname version src;
+      passthru = { inherit m2; };
+      buildInputs = with pkgs; [
+        leiningen
+        git
+        tree
+      ];
+      buildPhase = ''
+        mkdir classes
+        export HOME=$(pwd)
+        mkdir $HOME/.m2
+        tar -C /build --strip-components=1 -xf ${m2}
+        ls -lah
+        lein run -m clojure.main -c "(compile 'foo)"
+      '';
+      installPhase = ''
+        mkdir -pv $out
+        cp -r * $out
+      '';
+    };
+
+# in buildClojurePackage {
+#   pname = "test";
+#   version = "test";
+#   src = ./.;
+#   # m2Sha256 = "sha256-PGtBTUE/A6zEpasnmxRuzMnOCjGq2hsgCkHg3M5ZiAQ=";
+#   m2Sha256 = "";
+# }
+
+in { inherit fetchLeiningenTarball buildClojurePackage2; }
