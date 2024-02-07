55112d86c74d095ff6f5370f7f99c8d974b7319a
Kasper Gałkowski <kpg@posteo.net>
2024-02-07 21:17:29 +0100

    Explain the build system a little bit

--- nix-clj.nix
+++ nix-clj.nix
@@ -17,6 +17,46 @@ let
 
     nativeBuildInputs = [ jdk clojure ];
     propagatedBuildInputs = [ jdk clojure ] ++ deps;
+
+    # Clojure libraries seem to be usually built with deps.edn or Leiningen.
+    #
+    # I don't want to work with deps.edn, because it is hard-coded to make
+    # network requests on runtime. (Tried patching that out, but gave up, didn't
+    # dig too deep and don't remember what was blocking me... only remember it
+    # being something seemingly arbitrary - and some problems with it's fetching
+    # of git repositories).
+    #
+    # I remeber considering Leiningen, but discarding it because of
+    # bootstrapability issues - Leiningen is build using Leiningen. I tried
+    # compiling old versions from scratch but gave up after getting no real
+    # results.
+    #
+    # But maybe that's not a problem? Since the main thing that these tools are
+    # doing is exporting a classpath (after pulling binary blobs from maven), I
+    # can easily do it by hand. Here is a summary of the buildPhase:
+    #
+    # 1. Some libraries contain a little Java source code. Compile that first
+    #    (since the Clojure code might depend on that)
+    # 2. Now compile each namespace with clojure.core/compile.
+    #
+    # For that to work, some things need to happen:
+    #
+    # - A target directory for bytecode must exist. Default for Clojure is
+    #   "classes" - It's no problem to share it with any Java-produced code.
+    # - Any classes referenced in code must be present on the class path. Same
+    #   for required Clojure namespaces - clojure.core/require looks for that on
+    #   the classpath.
+    #
+    # Notes on the installPhase:
+    #
+    # - my Clojure doesn't seem to use the precombiled
+    #   bytecodes. It insists on always loading the source files, even when the
+    #   bytecode exists alongside it on the filesystem. I deduce this from the
+    #   clojure.core/require calls being just as slow. I might be doing something
+    #   wrong here... didn't dig into it too much. But decided to just prune the
+    #   sources from the uberjar, leaving only the bytecode. This was immediately
+    #   visible by loading being an order of magnitude faster.
+    # - Put the jar in $out/share/java for it to be found by nixpkgs setup hooks.
     
     buildPhase = let
       src' = if builtins.length patches > 0
