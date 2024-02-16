7b8452f91459b2e09673ff7183b5b1186c922e35
Kasper Gałkowski <kpg@posteo.net>
2024-02-16 23:06:13 +0100

    export java in flake

--- flake.nix
+++ flake.nix
@@ -10,9 +10,16 @@
         clojure = pkgs.callPackage ./nix-clj.nix {
           inherit (devpkgs) clojure jdk;
         };
+        java = pkgs.callPackage ./java.nix {
+          inherit (devpkgs) jdk;
+        };
       in {
         
         packages.clojure = clojure;
+
+        packages.java = java;
+
+        packages.maven = pkgs.maven;
 
         packages.cider = clojure.buildUberjar "cider" [
           clojure.pkgs.nrepl
