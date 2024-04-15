ffa900470030682c5368c38f41f9bf51e3e14daf
Kasper Gałkowski <kgalkowski@comscore.com>
2024-04-16 01:10:57 +0200

    openjdk9

---
+++ openjdk.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib, stdenv
+, fetchFromGitHub
+, openjdk-7
+, ... }:
+
+rec {
+
+  openjdk-8 = pkgs.jdk8.override {
+    openjdk8-bootstrap = openjdk-7 // {
+      home = "${openjdk-7}";
+    };
+  };
+
+  openjdk-9 = pkgs.callPackage ./openjdk/9.nix {
+    openjdk-bootstrap = openjdk-8;
+  };
+
+}
